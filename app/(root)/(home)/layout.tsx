755f1c8fbe34616a572fcf43a09783f70b072fad
KrieqqeR <burakcevizli@gmail.com>
2024-05-11 14:36:34 +0300

    Meeting room is done ...

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,13 @@
 import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "ROOM",
+  description: "Video calling app",
+  icons: '/icons/logo.svg'
+};
 
 const HomeLayout = ({children} : {children: ReactNode}) => {
   return (
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,12 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "ROOM",
+  description: "Video calling app",
+  icons: '/icons/logo.svg'
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
